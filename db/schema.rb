e4408cbcde2c9c638025736ac7ed7e19532dec3a
Brian DOstilio <briand787b@gmail.com>
2016-04-22 09:09:34 -0400

    changes aren't showing up for some reason

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421133107) do
+ActiveRecord::Schema.define(version: 20160422130156) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "name"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20160421133107) do
     t.text     "content"
     t.integer  "board_id"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "reference_post_id"
   end
 
   create_table "users", force: :cascade do |t|
